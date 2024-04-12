1c4849222ba1775e6d30ce4327dcaa348a3624c6
itdong <2901883544@qq.com>
2024-04-12 14:37:22 +0800

    后端接收请求后向mqtt发送前一天的时间功能完成

--- server/src/main/java/com/qgsg/config/mqtt/MqttSend.java
+++ server/src/main/java/com/qgsg/config/mqtt/MqttSend.java
@@ -1,56 +1,5 @@
 package com.qgsg.config.mqtt;
 
-//import lombok.extern.slf4j.Slf4j;
-//import org.eclipse.paho.client.mqttv3.MqttClient;
-//import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-//import org.eclipse.paho.client.mqttv3.MqttException;
-//import org.eclipse.paho.client.mqttv3.MqttMessage;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//import java.nio.charset.StandardCharsets;
-//
-//@Slf4j
-//public class MqttSend {
-//    @Autowired
-//    private MqttConfiguration mqttProperties;
-//    // 发布消息到MQTT服务器
-//    public Boolean publish(String payload) {
-//        log.info("准备发送：{}",payload);//数据传递无误
-//        try {
-//            // 创建一个MqttClient实例
-////            MqttClient mqttClient = new MqttClient("wss://uaac0158.ala.cn-hangzhou.emqxsl.cn:8084", "mqtt_qgsg_dev1");
-//            MqttClient mqttClient = new MqttClient(mqttProperties.getClientId(), mqttProperties.getClientId());
-//            // 创建一个MqttConnectOptions实例
-//            MqttConnectOptions connOp = new MqttConnectOptions();
-//            // 设置cleanSession为true
-//            connOp.setCleanSession(true);
-//            // 连接到MQTT服务器
-//            mqttClient.connect(connOp);
-//
-//            // 创建一个MqttMessage实例
-//            MqttMessage mqttMessage = new MqttMessage(payload.getBytes(StandardCharsets.UTF_8));
-//            // 设置Qos为1
-//            mqttMessage.setQos(1);
-//
-//            // 发布消息到MQTT服务器
-//            mqttClient.publish("emqx/esp32", mqttMessage);
-//
-//            // 断开与MQTT服务器的连接
-//            mqttClient.disconnect();
-//            log.info("发送成功");
-//            return true;
-//        } catch (MqttException e) {
-//            e.printStackTrace();
-//            log.info("发送失败");
-//            return false;
-//
-//        }
-//    }
-//
-//
-//
-//}
-
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
